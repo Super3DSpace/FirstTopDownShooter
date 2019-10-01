13b52609493630e25572e8e0e9944426eec8450e
Super3DSpace <jannisbrill@gmx.de>
2019-10-01 18:36:42 +0200

    Basic Player Movement

---
+++ First Top Down Shooter/Assets/The Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    private float y_speed;
+    private float x_speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        y_speed = Input.GetAxis("Vertical");
+        x_speed = Input.GetAxis("Horizontal");
+
+
+        Vector2 direction = new Vector2(x_speed, y_speed);
+        GetComponent<Rigidbody2D>().velocity = direction.normalized * speed ;
+
+    }
+}
